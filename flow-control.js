0af82d17afeb8cd6c7fcd36e06e608d381b66dd3
Lily Eichler <lilyeichler61@gmail.com>
2020-10-19 19:04:50 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,41 +1,36 @@
 function basicTeenager(age) {
-  if (age>=13 && age<=19) {
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!"
   }
 }
-
 function teenager(age) {
-  if (age>=13 && age=<19) {
-  return "You are a teenager!"
-}
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
   else {
-    return "You are not a teenager!"
+    return "You are not a teenager"
   }
 }
 
 function ageChecker(age) {
-  if (age>=13 && age=<19) {
-  return "You are a teenager!"
-}
-  else if (age<=12) {
-  return "You are a kid."
-}
-  else if (age>=20) {
-  return "You are an adult."
-}
-}
-
-function ternaryTeenager(age) {
-return age>=13 && age<=19 ? "You are a teenager!" : "You are not a teenager!"
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
+  else if (age <= 12) {
+    return "You are a kid"
+  }
+  else if (age >= 20) {
+    return "You are a grownup"
+  }
 }
 
 function switchAge(age) {
-switch (age)
-{
-case age>=13 && case<=19:
-return "You are a teenager!"
-break;
-default:
-return "You have an age."
-}
+  switch(age)
+  {
+    case age >= 13 && case <= 19:
+    return "You are a teenager"
+    break;
+    default:
+    return "You have an age"
+  }
 }
